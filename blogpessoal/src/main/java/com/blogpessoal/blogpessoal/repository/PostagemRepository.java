0fb2f87b4164a0425c6d3aac4ce145d0cad72c5e
Lavínia Amorim <101683059+larvakent@users.noreply.github.com>
2022-05-02 14:27:39 -0300

    Atualização

--- blogpessoal/src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.blogpessoal.blogpessoal.model.Postagem;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
 }
